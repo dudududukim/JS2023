07f5472d2df71caa9e047e8328d5988f8fa710a2
dudududukim <kdhluck@gmail.com>
2023-11-17 17:36:30 +0900

    ch5 finished(input source)

--- ch5/JS/form.js
+++ ch5/JS/form.js
@@ -3,7 +3,8 @@ let value;
 let number;
 btn.onclick = () => {
     value = document.querySelector("#product").value;
+    //form의 id요소로 접근하는 방법
     number = document.querySelector("#prod-num").value;
     document.querySelector("#product").textContent = "";
     document.querySelector("#prod-num").textContent = "";
-}   
+}
---
+++ ch5/JS/getFrom.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector("button");
+let selectedIndex;
+
+btn.onclick = ()=>{
+    selectedIndex = document.querySelector("#major").options.selectedIndex;
+    if(selectedIndex != 0){
+        alert(`${document.querySelector("#major").options[selectedIndex].textContent}is your major \n ${}`);
+    }else{
+        alert("please select your major");
+    }
+}
